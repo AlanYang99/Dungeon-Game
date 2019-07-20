3e0346eaf3cc6021280a76d02d5187b732c0aed3
bei <beichen7598@gmail.com>
2019-07-20 18:35:06 +1000

    wip: switch triggering

--- src/unsw/dungeon/Switch.java
+++ src/unsw/dungeon/Switch.java
@@ -1,7 +1,10 @@
 package unsw.dungeon;
 
+import java.util.List;
+
 public class Switch extends Entity {
 
+	private State state;
     /**
      * Create an switch positioned in square (x,y)
      * @param x
@@ -9,6 +12,7 @@ public class Switch extends Entity {
      */
     public Switch(Dungeon dungeon, int x, int y) {
         super(dungeon, x, y);
+        this.state = new Closed(); // assumes board cannot be initialized with boulder already on top of a switch
     }
 
 	@Override
@@ -16,7 +20,19 @@ public class Switch extends Entity {
 		if (item instanceof Door || item instanceof Exit || item instanceof Wall || item instanceof Switch) return false;
 		return true;
     }
-
+	
+	public void checkTriggered() {
+		List<Entity> entities = dungeon.getMap()[getX()][getY()];
+		
+		for (Entity e : entities) {
+			if (e instanceof Boulder) {
+				this.state.setTriggered();
+				break;
+			} else {
+				this.state.setUntriggered();
+			}
+		}
+	}
 
 
 }

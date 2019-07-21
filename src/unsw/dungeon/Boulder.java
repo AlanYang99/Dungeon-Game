b9b0bd52ea4b6a5d62e938a5bca0a05ed9171453
bei <beichen7598@gmail.com>
2019-07-21 13:55:52 +1000

    sharing for boulder

--- src/unsw/dungeon/Boulder.java
+++ src/unsw/dungeon/Boulder.java
@@ -13,33 +13,11 @@ public class Boulder extends MovableEntity {
 	public Boulder(Dungeon dungeon, int x, int y) {
 		super(dungeon, x, y);
 	}
-
+	
 	@Override
-	public boolean share(Entity item) {
-		if (item instanceof Switch)
-			return true;
+    public boolean share(Entity item) {
+    	if (item instanceof Switch) return true;
 		return super.share(item);
-	}
-
-    @Override
-    public void moveUp() {
-    	// trigger update all switches
     }
-    
-    @Override
-    public void moveDown() {
-    	// trigger update all switches
-    }
-    
-    @Override
-    public void moveLeft() {
-    	// trigger update all switches	
-    }
-    
-    @Override
-    public void moveRight() {
-    	// trigger update all switches	
-    }
-    
 
 }
